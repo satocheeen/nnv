92390a4a3c5a910413ed2beb5a802a5f1c2cd8bd
Satoko <satoko@suzuki.email.ne.jp>
2025-02-02 17:41:51 +0900

    ビルドエラー対処

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,7 +1,9 @@
-"use client"
 import MarkdownViewer from '../_components/common/MarkdownViewer';
 
 export default function PrivacyPolicyPage() {
+    if (typeof document === undefined) {
+        return <div />
+    }
     return (
         <MarkdownViewer mdfileName='privacy_policy.md' showBack />
     );
